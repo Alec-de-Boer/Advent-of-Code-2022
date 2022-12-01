6f1cc7db7fe83a93d0004576e2bded1f3659c7c4
Alec-de-Boer <alecdeboer@protonmail.com>
2022-12-01 16:30:54 +0100

    Final Solution Day 1 part 1 and 2

--- main.cpp
+++ main.cpp
@@ -5,39 +5,119 @@
 
 int main()
 {
-    //loop through input file.
-    std::string line;
-    int x = 0;
-    int y = 0;
-    std::ifstream inputFile("Input.txt", std::ios::in);
-    if (inputFile.is_open())
+
+    int userInput;
+    while (true)
     {
-        while (getline (inputFile,line))
+        std::cout << "Would you like solution 1 or 2 for day 1: ";
+        std::cin.clear();
+        std::cin >> userInput;
+
+        //Day 1 part 1 starts here.
+        if (userInput == 1)
         {
-            std::cout << line;
-            if (!line.empty())
+            //loop through input file.
+            std::string line;
+            int x = 0;
+            int y = 0;
+            std::ifstream inputFile("Input.txt", std::ios::in);
+            if (inputFile.is_open())
             {
-                int i = std::stoi(line);
-                int z = x + i;
-                x = z;
-                std::cout << "Not empty: total so far: " << x << std::endl;
-            }
-            else
-            {
-                std::cout << "This one is empty, total was: " << x << std::endl;
-                if(x > y)
+                while (getline (inputFile,line))
                 {
-                    y = x;
+                    std::cout << line;
+                    if (!line.empty())
+                    {
+                        int i = std::stoi(line);
+                        int z = x + i;
+                        x = z;
+                        std::cout << "Not empty: total so far: " << x << std::endl << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "This one is empty, total was: " << x << std::endl << std::endl;
+                        if(x > y)
+                        {
+                            y = x;
+                        }
+                        x = 0;
+                    }
                 }
-                x = 0;
+                inputFile.close();
             }
+            else std::cout << "Unable to open file" << std::endl;
+            std::cout << "The largest number was: " << y << std::endl;
+            break;
         }
-        inputFile.close();
+
+        //Day 1 part 2 starts here.
+        else if (userInput == 2)
+        {
+            //loop through input file.
+            std::string line;
+            int currentNumber = 0;
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int lastNumber = 0;
+            std::ifstream inputFile("Input.txt", std::ios::in);
+            if (inputFile.is_open())
+            {
+                while (getline (inputFile,line))
+                {
+                    std::cout << line;
+                    if (!line.empty())
+                    {
+                        int i = std::stoi(line);
+                        int z = currentNumber + i;
+                        currentNumber = z;
+                        std::cout << "Not empty: total so far: " << currentNumber << std::endl << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "This one is empty, total was: " << currentNumber << std::endl << std::endl;
+                        if(currentNumber > lastNumber)
+                        {
+                            //check if any values are empty and fill them first.
+                            if (firstNumber == 0)
+                            {
+                                firstNumber = currentNumber;
+                            }
+                            else if (secondNumber == 0)
+                            {
+                                secondNumber = currentNumber;
+                            }
+                            if (currentNumber > firstNumber)
+                            {
+                                lastNumber = secondNumber;
+                                secondNumber = firstNumber;
+                                firstNumber = currentNumber;
+                            }
+                            else if (currentNumber > secondNumber)
+                            {
+                                lastNumber = secondNumber;
+                                secondNumber = currentNumber;
+                            }
+                            else
+                            {
+                                lastNumber = currentNumber;
+                            }
+                        }
+                        currentNumber = 0;
+                    }
+                }
+                inputFile.close();
+            }
+            else std::cout << "Unable to open file" << std::endl;
+            std::cout << "The largest number was: " << firstNumber << " Followed by: " << secondNumber << " Followed by: " << lastNumber << std::endl;
+            std::cout << "Total: " << firstNumber + secondNumber + lastNumber << std::endl;
+
+            break;
+        }
+        else
+        {
+            std::cout << "Please enter a valid number";
+        }
     }
-    else std::cout << "Unable to open file" << std::endl;
-
-    std::cout << "The largest number was: " << y << std::endl;
-
     //prevents application from automatically closing and waiting for user-input.
     std::cout << "Press any key to exit." << std::endl;
     std::cin.ignore();
