6d58cc3f9ae07583b8ad25024f7bd4008dbd0efb
Alec-de-Boer <alecdeboer@protonmail.com>
2022-12-01 15:41:22 +0100

    Final solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-#include <boost/thread.hpp>
+#include <fstream>
+#include <filesystem>
+#include <vector>
 
 int main()
 {
+    //loop through input file.
+    std::string line;
+    int x = 0;
+    int y = 0;
+    std::ifstream inputFile("Input.txt", std::ios::in);
+    if (inputFile.is_open())
+    {
+        while (getline (inputFile,line))
+        {
+            std::cout << line;
+            if (!line.empty())
+            {
+                int i = std::stoi(line);
+                int z = x + i;
+                x = z;
+                std::cout << "Not empty: total so far: " << x << std::endl;
+            }
+            else
+            {
+                std::cout << "This one is empty, total was: " << x << std::endl;
+                if(x > y)
+                {
+                    y = x;
+                }
+                x = 0;
+            }
+        }
+        inputFile.close();
+    }
+    else std::cout << "Unable to open file" << std::endl;
+
+    std::cout << "The largest number was: " << y << std::endl;
 
     //prevents application from automatically closing and waiting for user-input.
     std::cout << "Press any key to exit." << std::endl;
